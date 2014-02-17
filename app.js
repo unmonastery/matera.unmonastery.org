6eff6972917f0bddfc70d534a2c224c324c0dadc
elf Pavlik <perpetual-tripper@wwelves.org>
2014-02-17 20:45:47 +0100

    started working on router

--- app.js
+++ app.js
@@ -3,32 +3,45 @@ $(function(){
   var unMons = [
     {
       "name": "Kei Kreutler",
+      "slug": "kei-kreutler",
       "email": "kei@ourmachine.net",
       "md5email": "d87e3342938497fd997c8fdc00dfe891"
     },
     {
       "name": "Ben Vickers",
-      "email": "ben@vickers.tv"
+      "slug": "ben-vickers",
+      "email": "ben@vickers.tv",
+      "md5email": "6fd3f132175cea545d2bb8eb9e05518a"
     },
     {
       "name": "Cristiano Siri",
-      "email": "cristiano.siri@gmail.com"
+      "slug": "cristiano-siri",
+      "email": "cristiano.siri@gmail.com",
+      "md5email": "c6dc44fe9be0fb0556fdfb6abf6b4fe8"
     },
     {
       "name": "Bembo Davies",
-      "email": "bem.davies@gmail.com"
+      "slug": "bembo-davies",
+      "email": "bem.davies@gmail.com",
+      "md5email": "e71846d61c43b5b9a1469b9632e6870"
     },
     {
       "name": "elf Pavlik",
-      "email": "perpetual-tripper@wwelves.org"
+      "slug": "elf-pavlik",
+      "email": "perpetual-tripper@wwelves.org",
+      "md5email": "f8a7b786c9f7b74164c6503173e92495"
     },
     {
       "name": "Katalin Hausner",
-      "email": "katalinhausel@gmail.com"
+      "slug": "katalin-hausner",
+      "email": "katalinhausel@gmail.com",
+      "md5email": "b945907a201cea91efece7213e3e27b4"
     },
     {
       "name": "Marc Schneider",
-      "email": "marc@mirelsol.org"
+      "slug": "marc-schneider",
+      "email": "marc@mirelsol.org",
+      "md5email": "7a3fd0312c38ae89119e9ae311740021"
     }
   ];
 
@@ -49,6 +62,27 @@ $(function(){
   var Crew = Backbone.Collection.extend({
     model: Person
   });
+
+  var crew = new Crew(unMons);
+
+
+  var Router = Backbone.Router.extend({
+    routes: {
+      '': 'root',
+      'people': 'people',
+      'people/:slug': 'person',
+      'projects': 'projects',
+      'projects/:slug': 'project'
+    },
+
+    person: function(name){
+      var profile = new Profile({ model: crew.findWhere({slug: name}) });
+    }
+  });
+
+  Backbone.history.start({ pushState: true });
+
+  var router = new Router();
 
   var AgentMenu = Backbone.View.extend({
     el: '#agentMenu',
@@ -76,6 +110,10 @@ $(function(){
   var CrewView = Backbone.View.extend({
     el: '#crew',
 
+    events: {
+      'click a': 'showProfile'
+    },
+
     initialize: function(){
       _.bindAll(this, 'render');
       this.render();
@@ -85,6 +123,12 @@ $(function(){
       this.collection.each(function(person){
         this.$el.append(JST.nameLink(person.toJSON()));
       }.bind(this));
+    },
+
+    showProfile: function(event){
+      window.dbg = event;
+      event.preventDefault();
+      router.navigate(event.target.attributes.href.value, { trigger: true });
     }
   });
 
@@ -103,15 +147,14 @@ $(function(){
   });
 
   var agent = new Person();
-  var crew = new Crew(unMons);
   var agentMenu = new AgentMenu({ model: agent });
   var crewVeiw = new CrewView({ collection: crew });
-  var profile = new Profile({ model: crew.at(0) });
 
   // debug
   window.un = {
     agent: agent,
-    crew: crew
+    crew: crew,
+    router: router
   };
 
   var login = function(assertion){
