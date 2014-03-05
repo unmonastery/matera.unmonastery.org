7f3e42d2fbde02e956ab33b2ecec6a0928934c61
elf Pavlik <perpetual-tripper@wwelves.org>
2014-03-05 07:56:34 +0100

    updated exemple deamon's config (forgotten in prev commit)

--- config/daemon.example.js
+++ config/daemon.example.js
@@ -1,5 +1,6 @@
 module.exports = {
   db: 'dev.ldb',
+  port: '9000',
   audiences: ['http://localhost:8080', 'http://unmonastery.dev', 'http://matera.unmonastery.dev'],
   secrets: {
     cookie: 'Thalugnesfit0drowAbJaskEbyocyut',
