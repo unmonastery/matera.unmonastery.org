023b1b6e0bba76ae0c0a28ce6dceb8ffbfce1117
elf Pavlik <perpetual-tripper@wwelves.org>
2014-03-05 07:22:41 +0100

    updated example config for daemon

--- config/daemon.example.js
+++ config/daemon.example.js
@@ -1,3 +1,8 @@
 module.exports = {
-  audiences: ['http://localhost:8080', 'http://unmonastery.dev', 'http://matera.unmonastery.dev']
+  db: 'dev.ldb',
+  audiences: ['http://localhost:8080', 'http://unmonastery.dev', 'http://matera.unmonastery.dev'],
+  secrets: {
+    cookie: 'Thalugnesfit0drowAbJaskEbyocyut',
+    session: 'RovFosithyltyojdykCadWysdurt2onn'
+  }
 };
