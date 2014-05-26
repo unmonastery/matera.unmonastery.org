50a8ab9538a61072bff226f6639ee837aec2d556
elf Pavlik <perpetual-tripper@wwelves.org>
2014-05-26 16:48:39 +0200

    created task for fetching post from ghost

--- Gruntfile.js
+++ Gruntfile.js
@@ -92,4 +92,96 @@ module.exports = function(grunt){
       });
     }
   });
+
+  // fetches posts from ghost
+  grunt.registerTask('ghost', function(){
+    var superagent = require('superagent');
+    var concat = require('concat-stream');
+    var pickup = require('pickup');
+    var async = require('async');
+    var _ = require('lodash');
+    var feeds = [
+      { lang: 'en', url: 'http://en-news.unmonastery.org/rss/' },
+      { lang: 'it', url: 'http://it-news.unmonastery.org/rss/' }
+    ];
+
+    var lg = require('levelgraph');
+    var lgJSONLD = require('levelgraph-jsonld');
+    var config = require('./config/daemon');
+    var db = lgJSONLD(lg(config.db));
+
+    var done = this.async();
+
+    function ldfize(blog, lang) {
+      return _.map(blog.entries, function(post){
+        var result = {
+          "@context": {
+            "@vocab": "http://schema.org/",
+            "text": { "@container": "@language" }
+          },
+          "@type": "BlogPosting",
+          "@id": post.link,
+          name: post.title,
+          text: {},
+          dateModified: new Date(post.updated).toISOString()
+        };
+        result.text[lang] = post.summary;
+        return result;
+      });
+    }
+
+    function fetchFeed(memo, feed, callback){
+      superagent.get(feed.url)
+        .pipe(pickup())
+        .pipe(concat(function(data){
+          var blog = JSON.parse(data.toString());
+          var posts = ldfize(blog, feed.lang);
+          var error = null;
+          if(posts.length === 0) error = new Error('empty feed');
+          callback(error, _.flatten([memo, posts]));
+        }));
+    }
+
+    function putPost(item, callback){
+      db.jsonld.put(item, function(err, obj){
+        if(err)
+          callback(err);
+        else
+          callback();
+      });
+    }
+
+    function delPost(id, callback){
+      db.jsonld.del(id, function(err){
+        if(err)
+          callback(err);
+        else
+          callback();
+      });
+    }
+
+    function updateDb(err, posts){
+      if(err) return console.log(err);
+      db.search({
+        subject: db.v('x'),
+        predicate: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
+        object: 'http://schema.org/BlogPosting'
+      }, function(err, list){
+        console.log('deleting', list);
+        var ids = _.map(list, function(item){ return item.x; });
+        // delete all posts
+        async.each(ids, delPost, function(err){
+          if(err) return console.log(err);
+          // recreate posts from latest feed
+          async.each(posts, putPost, function(err){
+            if(err) return console.log(err);
+            done();
+          });
+        });
+      });
+    }
+
+    async.reduce(feeds, [], fetchFeed, updateDb);
+
+  });
 };
